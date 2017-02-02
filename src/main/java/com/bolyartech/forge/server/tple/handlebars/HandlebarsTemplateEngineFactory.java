fcddac35169db5926417649733537a7ca4bee702
Ognyan Bankov <ogibankov@gmail.com>
2017-02-02 08:43:30 +0200

    adding cache

--- src/main/java/com/bolyartech/forge/server/tple/handlebars/HandlebarsTemplateEngineFactory.java
+++ src/main/java/com/bolyartech/forge/server/tple/handlebars/HandlebarsTemplateEngineFactory.java
@@ -3,8 +3,15 @@ package com.bolyartech.forge.server.tple.handlebars;
 import com.bolyartech.forge.server.misc.TemplateEngine;
 import com.bolyartech.forge.server.misc.TemplateEngineFactory;
 import com.github.jknack.handlebars.Handlebars;
+import com.github.jknack.handlebars.Template;
+import com.github.jknack.handlebars.cache.GuavaTemplateCache;
 import com.github.jknack.handlebars.io.ClassPathTemplateLoader;
 import com.github.jknack.handlebars.io.TemplateLoader;
+import com.github.jknack.handlebars.io.TemplateSource;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+
+import java.util.concurrent.TimeUnit;
 
 
 public class HandlebarsTemplateEngineFactory implements TemplateEngineFactory {
@@ -21,7 +28,11 @@ public class HandlebarsTemplateEngineFactory implements TemplateEngineFactory {
         templateLoader.setPrefix(mTemplatePathPrefix);
         templateLoader.setSuffix(null);
 
-        mHandlebars = new Handlebars(templateLoader);
+        Handlebars handlebarsTmp = new Handlebars(templateLoader);
+        Cache<TemplateSource, Template> cache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES)
+                .maximumSize(1000).build();
+
+        mHandlebars = handlebarsTmp.with(new GuavaTemplateCache(cache));
     }
 
 
